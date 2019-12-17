c23ee626b89adcc90d3d9996b635a0dd1bf12445
nithyamn <nithya.mn999@gmail.com>
2019-12-17 19:07:46 +0530

    Update single.conf.js

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
-  user: process.env.BROWSERSTACK_USERNAME || 'nithyamani3',
-  key: process.env.BROWSERSTACK_ACCESS_KEY || 'P4JKysg5WuchQxBfKQu1',
+  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
 
   updateJob: false,
   specs: [
